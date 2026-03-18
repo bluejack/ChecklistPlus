7be0f40e54e345732692483104e24dc0f4cc5cc6
Blunt Jackson <blunt.jackson@invicti.com>
2026-03-18 13:17:50 -0500

    Refactor to support external hyperlinks and improve robustness of all linking

--- Checklist+.py
+++ Checklist+.py
@@ -17,34 +17,41 @@ def find_ckl_file(view, name):
     return None
 
 
-class FileLinkListener(sublime_plugin.ViewEventListener):
+class FileLinkHighlighter(sublime_plugin.ViewEventListener):
+    """Dynamically styles /name/ references as links only when name.ckl exists."""
 
-    def __init__(self, view):
-        super().__init__(view)
-        self.phantom_set = sublime.PhantomSet(self.view, "file_links")
+    @classmethod
+    def is_applicable(cls, settings):
+        syntax = settings.get('syntax', '')
+        return 'Checklist+' in syntax
 
     def on_load(self):
-        self.update_phantoms()
+        self.update_links()
 
-    def on_modified(self):
-        self.update_phantoms()
+    def on_post_save_async(self):
+        self.update_links()
 
-    def update_phantoms(self):
-        phantoms = []
-        regions = self.view.find_all(r'/([^/\n]+)/')
-        for r in regions:
+    def on_modified_async(self):
+        cc = self.view.change_count()
+        sublime.set_timeout(lambda: self._debounced_update(cc), 400)
+
+    def _debounced_update(self, change_count):
+        if self.view.change_count() == change_count:
+            self.update_links()
+
+    def update_links(self):
+        resolved_regions = []
+        for r in self.view.find_all(r'/([^/\n]+)/'):
             name = self.view.substr(r).strip('/')
-            resolved = find_ckl_file(self.view, name)
-            if resolved:
-                html = f'<a href="open:{resolved}">↗</a>'
-                phantoms.append(
-                    sublime.Phantom(r, html, sublime.LAYOUT_INLINE, self.on_click)
-                )
-        self.phantom_set.update(phantoms)
+            if find_ckl_file(self.view, name):
+                resolved_regions.append(r)
+        self.view.add_regions(
+            "file_links",
+            resolved_regions,
+            scope="markup.link",
+            flags=sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SOLID_UNDERLINE
+        )
 
-    def on_click(self, href):
-        _, path = href.split(":", 1)
-        self.view.window().open_file(path)
 
 class OpenCklUnderCursorCommand(sublime_plugin.TextCommand):
 
@@ -54,6 +61,15 @@ class OpenCklUnderCursorCommand(sublime_plugin.TextCommand):
         line_text = self.view.substr(line)
         col = click_point - line.begin()
 
+        # Check markdown links first
+        for m in LINK_RE.finditer(line_text):
+            link_start = m.start(1) - 1  # include the [
+            link_end = m.end(1) + 1      # include the ]
+            if link_start <= col < link_end:
+                sublime.run_command("open_url", {"url": m.group(2)})
+                return
+
+        # Fall through to file link logic
         for m in re.finditer(r'/([^/\n]+)/', line_text):
             if m.start() <= col < m.end():
                 name = m.group(1)
@@ -70,3 +86,6 @@ class OpenCklUnderCursorCommand(sublime_plugin.TextCommand):
 
     def want_event(self):
         return True
+
+LINK_RE = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
+
